5b21dafcf71b2b1413f09c946ecd04de1dee7327
hely111 <105413774+hely111@users.noreply.github.com>
2022-05-15 00:16:35 +0800

    Add files via upload

--- src/main/java/Del_imf/AnnDel.java
+++ src/main/java/Del_imf/AnnDel.java
@@ -6,35 +6,25 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-/**
- * Servlet implementation class AnnDel
- */
+import java.io.PrintWriter;
+@WebServlet("/AnnDel")
 public class AnnDel extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public AnnDel() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doPost(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");//返回text
+		int id=Integer.parseInt(request.getParameter(""));//传入公告编号
+		DeleteDB Del =new DeleteDB();
+		PrintWriter out = response.getWriter();
+		out.print(Del.annDel(id));//返回前端，成功0，失败1
+		out.flush();
+		out.close();
 	}
-
 }
--- src/main/java/Del_imf/StuDel.java
+++ src/main/java/Del_imf/StuDel.java
@@ -6,16 +6,25 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-
+import java.io.PrintWriter;
+@WebServlet("/StuDel")
 public class StuDel extends HttpServlet {
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private static final long serialVersionUID = 1L;
+    public StuDel() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		doPost(request,response);
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");//返回text
+		int id=Integer.parseInt(request.getParameter(""));//传入公告编号
+		DeleteDB Del =new DeleteDB();
+		PrintWriter out = response.getWriter();
+		out.print(Del.stuDel(id));//返回前端，成功0，失败1
+		out.flush();
+		out.close();
 	}
-
 }
