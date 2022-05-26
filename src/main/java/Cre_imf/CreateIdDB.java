0a598ea06f52b8f36247b26cd3828fd605cd9047
hely111 <105413774+hely111@users.noreply.github.com>
2022-05-27 00:13:59 +0800

    Add files via upload

--- src/main/java/Cre_imf/CreateIdDB.java
+++ src/main/java/Cre_imf/CreateIdDB.java
@@ -24,14 +24,15 @@ public class CreateIdDB {
 			System.out.println("教职工id提取失败");
 		}
 		try {
-			stmt=con.prepareStatement("updata createid set ID_TEA=?");
-			stmt.setInt(1, Id+1);
+			stmt=con.prepareStatement("update createid set ID_TEA=? where ID_Mark=1");
+			Id++;
+			stmt.setInt(1, Id);
 			stmt.executeUpdate();
 		}
 		catch(Exception e) {
 			System.out.println("教职工id修改sql语句修改失败");
 		}
-		return Id;
+		return Id-1;
 	}
 	public int CreateForIdDB() throws SQLException{
 		int Id=0;
@@ -51,14 +52,15 @@ public class CreateIdDB {
 			System.out.println("论坛id提取失败");
 		}
 		try {
-			stmt=con.prepareStatement("updata createid set ID_FOR=?");
-			stmt.setInt(1, Id+1);
+			stmt=con.prepareStatement("update createid set ID_FOR=? where ID_Mark=1");
+			Id++;
+			stmt.setInt(1,Id);
 			stmt.executeUpdate();
 		}
 		catch(Exception e) {
 			System.out.println("论坛id修改sql语句修改失败");
 		}
-		return Id;
+		return Id-1;
 	}
 	public int CreateAnnIdDB() throws SQLException{
 		int Id=0;
@@ -78,20 +80,21 @@ public class CreateIdDB {
 			System.out.println("公告id提取失败");
 		}
 		try {
-			stmt=con.prepareStatement("updata createid set ID_ANN=?");
-			stmt.setInt(1, Id+1);
+			stmt=con.prepareStatement("update createid set ID_ANN=? where ID_Mark=1");
+			Id++;
+			stmt.setInt(1, Id);
 			stmt.executeUpdate();
 		}
 		catch(Exception e) {
 			System.out.println("公告id修改sql语句修改失败");
 		}
-		return Id;
+		return Id-1;
 	}
 	public int CreateStuIdDB(String ClassName) throws SQLException{
-		int Id=20200000;
+		int Id=200000000;//前两位：入学时间 后四位班级编号 最后两位根据班级里面计数器得来
 		con=DBConnection.getConnection();
 		try {
-			stmt=con.prepareStatement("select ID_STU,CLA_ID,PRO_ID from class_inf where CLA_NA=?");
+			stmt=con.prepareStatement("select ID_STU,CLA_ID from class_inf where CLA_NA=? ");
 			stmt.setString(1,ClassName);
 			rs=stmt.executeQuery();
 		}
@@ -100,10 +103,19 @@ public class CreateIdDB {
 		}
 		try {
 			rs.next();
-			Id+=(Integer.parseInt(rs.getString("ID_STU"))+100*Integer.parseInt(rs.getString("CLA_ID"))+1000*Integer.parseInt(rs.getString("PRO_ID")));
+			Id+=(100*Integer.parseInt(rs.getString("CLA_ID"))+Integer.parseInt(rs.getString("ID_STU")));
 		}
 		catch(Exception e) {
 			System.out.println("学生id查询数据读取失败");
+		}
+		try {
+			stmt=con.prepareStatement("update class_inf set ID_STU=? where CLA_NA=?");
+			stmt.setInt(1,Integer.parseInt(rs.getString("ID_STU"))+1);
+			stmt.setString(2,ClassName);
+			stmt.executeUpdate();
+		}
+		catch(Exception e) {
+			System.out.println("班级id修改保存失败");
 		}
 		return Id;
 	}
